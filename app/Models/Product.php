848000cc0f98a2a43c59b0fc09c6119217a8cb39
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-15 21:44:08 +0700

    feat(model): Add relationships and soft deletes to Product model
    Introduced SoftDeletes to the Product model and defined fillable attributes. Added relationships to Category, Merchant (with pivot stock), and Warehouse (with pivot stock) for improved data management and Eloquent querying.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     //
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'description',
+        'photo',
+        'price',
+        'category_id'
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function merchants(){
+        return $this->belongsToMany(Merchant::class, 'merchant_products')
+        ->withPivot('stock')
+        ->withTimestamps();
+    }
+
+    public function warehouses(){
+        return $this->belongsToMany(Warehouse::class,'warehouse_products')
+        ->withPivot('stock')
+        ->withTimestamps();
+    }
 }
