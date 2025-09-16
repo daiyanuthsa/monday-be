27864543f734c65387754cf7a8b707462c6343f8
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-16 17:47:44 +0700

    feat(transaction): add attribute, relation, and accessor

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'sub_total',
+        'tax_total',
+        'grand_total',
+        'merchant_id',
+    ];
+
+    public function merchant()
+    {
+        return $this->belongsTo(Merchant::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(TransactionProduct::class);
+    }
 }
