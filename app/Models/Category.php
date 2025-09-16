80c5ce692c2cba3eb45746c943d6e7072f49ae64
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-16 15:32:24 +0700

    feat(category): modify model, add fillable attribute, relation and accessor

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ["name","photo","tagline"];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function photo(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => url(Storage::url($value)),
+        );
+    }
 }
