620768aee027b426b85d5908a99d0b2d3f7927c0
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-16 17:33:46 +0700

    feat(warehouse): add attribute, relation, and accessor

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Warehouse extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable=['name','address','photo','phone'];
+
+    public function products(){
+        return $this->belongsToMany(Product::class,'warehouse_products')
+        ->withPivot('stock')
+        ->withTimestamps();
+    }
+
+    public function photo(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => url(Storage::url($value)),
+        );
+    }
 }
