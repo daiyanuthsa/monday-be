283b5dc6af53124b18bb5f21cc466fbf4b3bf00f
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-16 17:34:04 +0700

    feat(merchant): add attribute, relation, and accessor

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -2,9 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 class Merchant extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'photo',
+        'phone',
+        'keeper_id'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'merchant_products')
+            ->withPivot('stock')
+            ->withPivot('warehouse_id')
+            ->withTimestamps();
+    }
+
+    public function keeper()
+    {
+        return $this->belongsTo(User::class, 'keeper_id');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function photo(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => url(Storage::url($value)),
+        );
+    }
+
 }
